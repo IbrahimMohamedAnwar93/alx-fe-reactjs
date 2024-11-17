4489c7474566f1978c1f6375f037aa961619f923
Ibrahim Mohamed <ibrahim.m.anwar@gmail.com>
2024-11-17 22:46:36 +0200

    task 2 done

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';  
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing Application</h1>
+        <AddRecipeForm />
+        <RecipeList />
+        <Routes>  
+          <Route path="/" element={<RecipeList />} />  
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 

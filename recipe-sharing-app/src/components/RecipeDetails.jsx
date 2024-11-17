df0b2ffc9dee9db6797863f4131e6c29e5de689f
Ibrahim Mohamed <ibrahim.m.anwar@gmail.com>
2024-11-17 21:49:18 +0200

    Enhancing the Recipe Sharing Application with Detailed Recipe Management done

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 import { useParams } from 'react-router-dom';

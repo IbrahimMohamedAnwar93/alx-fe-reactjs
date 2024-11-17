c9fab65c51cbb578b6cd107d53cbe0c3ce59e661
Ibrahim Mohamed <ibrahim.m.anwar@gmail.com>
2024-11-17 23:04:17 +0200

    task 3 done

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,10 @@
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
-  recipes: [],  
-  searchTerm: '',  
-  filteredRecipes: [],  
-  
+  recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (recipeId) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
   updateRecipe: (updatedRecipe) => set(state => ({
@@ -14,12 +14,15 @@ const useRecipeStore = create(set => ({
   })),
   
   setSearchTerm: (term) => set({ searchTerm: term }),
+  
   filterRecipes: () => set(state => ({
     filteredRecipes: state.recipes.filter(recipe =>
-      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()) ||  // Filter by recipe title
-      recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(state.searchTerm.toLowerCase()))  // Filter by ingredients
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()) ||  
+      recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(state.searchTerm.toLowerCase()))  
     ),
   })),
+
+  setRecipes: (newRecipes) => set({ recipes: newRecipes, filteredRecipes: newRecipes }) 
 }));
 
 export default useRecipeStore;

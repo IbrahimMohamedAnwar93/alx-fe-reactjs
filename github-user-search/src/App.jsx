8f3109700982b50196d7c7436f84a936a94962bb
Ibrahim Mohamed <ibrahim.m.anwar@gmail.com>
2024-12-22 20:58:19 +0200

    Initial commit: Set up GitHub User Search app

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>GitHub User Search</h1>
+      <p>Start searching for GitHub users...</p>
+    </div>
+  );
+}
+
+export default App;
